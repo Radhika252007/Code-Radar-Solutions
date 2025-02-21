71a4430dce72aa19256a7ddd5dd3b90093def252
Radhika252007 <mukeshmadaan2025@gmail.com>
2025-02-21 17:47:24 +0530

    Solution to Count Words in a String

--- Count Words in a String@@676c039d80e1714600f67646/code.c
+++ Count Words in a String@@676c039d80e1714600f67646/code.c
@@ -1,17 +1,42 @@
 // Your code here...
+// #include <stdio.h>
+// int main(){
+//     char str[1000];
+//     fgets(str,1000,stdin);
+//     int i=0,j=0;
+//     while(str[i]==' ')i++;
+//     while(str[j++]=str[i++]);
+//     int count = 1;
+//     for(int k=0;str[k]!='\0';k++){
+//         if(str[k]==' '){
+//             count++;
+//         }
+//     }
+//     printf("%d",count);
+//     return 0;
+// }
 #include <stdio.h>
 int main(){
     char str[1000];
-    fgets(str,1000,stdin);
-    int i=0,j=0;
-    while(str[i]==' ')i++;
-    while(str[j++]=str[i++]);
-    int count = 1;
-    for(int k=0;str[k]!='\0';k++){
-        if(str[k]==' '){
+    fgets(str, 1000, stdin);
+    int i = 0, j = 0;
+    
+    // Skip leading spaces
+    while (str[i] == ' ') i++;
+    
+    // Shift characters to remove leading spaces
+    while (str[j++] = str[i++]);
+    
+    // Count words
+    int count = 0;
+    for (int k = 0; str[k] != '\0'; k++) {
+        if (str[k] == ' ' && str[k+1] != ' ' && str[k+1] != '\0') {
             count++;
         }
     }
-    printf("%d",count);
+    // Increment count for the first word (if any)
+    if (str[0] != '\0') count++;
+
+    printf("%d", count);
     return 0;
 }
