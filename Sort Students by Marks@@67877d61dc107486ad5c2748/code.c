6edbafa069093fd617da2f0aade436ab6249250c
Radhika252007 <mukeshmadaan2025@gmail.com>
2025-02-24 19:05:43 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -5,10 +5,10 @@ struct Student{
     char name[200];
     float marks;
 };
-void swap(struct Student a, struct Student b){
-    struct Student temp = a;
-    a = b;
-    b = temp ;
+void swap(struct Student *a, struct Student *b){
+    struct Student temp = *a;
+    *a = *b;
+    *b = temp ;
 }
 
 int main(){
@@ -21,7 +21,7 @@ int main(){
     for(int i=0;i<N-1;i++){
         for(int j=0;j<N-i-1;j++){
             if(sort[j].marks<sort[j+1].marks){
-                swap(sort[j],sort[j+1]);
+                swap(&sort[j],&sort[j+1]);
             }
         }
     }
