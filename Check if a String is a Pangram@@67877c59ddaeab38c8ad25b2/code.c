3574c2ca060a233a6761713ece7e84d30a5367ec
Radhika252007 <mukeshmadaan2025@gmail.com>
2025-02-27 12:52:34 +0530

    Solution to Check if a String is a Pangram

---
+++ Check if a String is a Pangram@@67877c59ddaeab38c8ad25b2/code.c
@@ -0,0 +1,26 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char ch[300];
+    fgets(ch,300,stdin);
+    char freq[26]={0};
+    int index;
+    for(int i=0;ch[i]!='\0';i++){
+        if(ch[i]>='A'&&ch[i]<='Z'){
+            index = ch[i]-'A';
+            freq[index]++;
+        }
+        else if(ch[i]>='a'&&ch[i]<='z'){
+            index = ch[i]-'a';
+            freq[index]++;
+        }
+    }
+    for(int i=0;i<index;i++){
+        if(freq[i]!=1){
+            printf("No");
+            return 0;
+        }
+    }
+    printf("Yes");
+}
