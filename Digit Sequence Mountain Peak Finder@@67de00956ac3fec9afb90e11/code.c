92a8dc9f9de5e2947ba897b1390fd3e389368181
Radhika252007 <mukeshmadaan2025@gmail.com>
2025-03-29 18:37:17 +0530

    Solution to Digit Sequence Mountain Peak Finder

--- Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -5,7 +5,7 @@ int mountainPeak(int n){
     if(len%2==0){
         return -1;
     }
-    int i =0;j=len-1;
+    int i =0,j=len-1;
     while(i<=j){
         if(n_str[i]!=n_str[j]){
             return -1;
