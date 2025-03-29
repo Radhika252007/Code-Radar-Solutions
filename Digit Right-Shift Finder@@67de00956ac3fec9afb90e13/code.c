d71cd3a3d12386f38e236f57ff712022b48c5f82
Radhika252007 <mukeshmadaan2025@gmail.com>
2025-03-29 18:27:34 +0530

    Solution to Digit Right-Shift Finder

--- Digit Right-Shift Finder@@67de00956ac3fec9afb90e13/code.c
+++ Digit Right-Shift Finder@@67de00956ac3fec9afb90e13/code.c
@@ -2,8 +2,8 @@
 int digitRightShift(int n){
     char n_str[50];
     int len = sprintf(n_str,"%d",n);
-    char temp = n_str[len];
-    for(int i=len-1;i>=0;i--){
+    char temp = n_str[len-1];
+    for(int i=len-2;i>=0;i--){
         n_str[i+1]=n_str[i];
     }
     n_str[0]=temp;
