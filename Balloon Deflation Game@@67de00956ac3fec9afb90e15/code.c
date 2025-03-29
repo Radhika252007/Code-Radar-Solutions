90d94459f958bf6a8a4dd50e11c409ef22623b06
Radhika252007 <mukeshmadaan2025@gmail.com>
2025-03-29 17:53:30 +0530

    Solution to Balloon Deflation Game

---
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -0,0 +1,31 @@
+void bubbleSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;          
+            }
+        }
+    }
+}
+void deflateBalloons(int arr[],int n){
+    bubbleSort(arr,n);
+    int min = arr[0];
+    for(int i=0;i<n;i++){
+        int count =0;
+        for(int j=0;j<n;j++){
+            if(arr[j]>0){
+                arr[j]=arr[j]-min;
+                count++;
+            }
+        }
+        printf("%d\n",count);
+        for(int k=0;k<n;k++){
+            if(arr[k]!=0){
+                min = arr[k];
+                break;
+            }
+        }
+    }
+}
